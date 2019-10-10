603e1aea4ac8cd97e1d50a0b99dd1257973c4613
Dilip Kola <dilip@tensult.com>
2019-10-10 10:07:01 +0530

    Update dialog component

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
 import { DialogExampleComponent } from '../dialog-example/dialog-example.component';
 
 @Component({
